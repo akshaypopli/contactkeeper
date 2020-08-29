2fb3590f2b12ad24ca4b35f00e8f769f76bbf4c9
Mac <mac@Akshays-Air.local>
2020-08-29 02:12:02 -0400

    deployment changes

--- contact-keeper-api/server.js
+++ contact-keeper-api/server.js
@@ -5,6 +5,8 @@ const auth = require('./routes/auth');
 
 const connectDB = require('./config/db'); 
 
+const path = require('path');
+
 const app = express();
 
 // connect Database
@@ -12,12 +14,20 @@ connectDB();
 
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.json({msg: 'welcome to contact keeper api'}));
-
 //routes
 app.use('/api/users',   users);
 app.use('/api/auth',   auth);
 app.use('/api/contacts',   contacts);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('frontend/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  }
 
 const PORT = process.env.PORT || 5000;
 
